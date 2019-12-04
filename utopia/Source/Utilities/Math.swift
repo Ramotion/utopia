64d0f96f83a5f52d17eb58c7c2b5a095a2b5947b
igor.k <igor.k@ramotion.agency>
2019-12-04 17:10:13 +0300

    lerp UIColor & CGRect

--- utopia/Source/Utilities/Math.swift
+++ utopia/Source/Utilities/Math.swift
@@ -5,6 +5,24 @@ public enum Math {
   public static func lerp<T: FloatingPoint>(from: T, to: T, progress: T) -> T {
     return from + progress * (to - from);
   }
+  
+  public static func lerp(from: UIColor, to: UIColor, progress: CGFloat) -> UIColor {
+    let rgba1 = from.rgba
+    let rgba2 = to.rgba
+    let r = lerp(from: rgba1.r, to: rgba2.r, progress: progress)
+    let g = lerp(from: rgba1.g, to: rgba2.g, progress: progress)
+    let b = lerp(from: rgba1.b, to: rgba2.b, progress: progress)
+    let a = lerp(from: rgba1.a, to: rgba2.a, progress: progress)
+    return UIColor(red: r, green: g, blue: b, alpha: a)
+  }
+  
+  public static func lerp(from: CGRect, to: CGRect, progress: CGFloat) -> CGRect {
+    let x = lerp(from: from.origin.x, to: to.origin.x, progress: progress)
+    let y = lerp(from: from.origin.y, to: to.origin.y, progress: progress)
+    let w = lerp(from: from.size.width, to: to.size.width, progress: progress)
+    let h = lerp(from: from.size.height, to: to.size.height, progress: progress)
+    return CGRect(x: x, y: y, width: w, height: h)
+  }
 }
 
 public enum Progress {
